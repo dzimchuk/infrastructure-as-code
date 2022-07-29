96a3ae79451a5c9155783dd8d7c385518499e142
Andrei Dzimchuk <3907708+dzimchuk@users.noreply.github.com>
2022-07-29 16:50:43 +0300

    added a simple workflow

--- azure/terraform/main.tf
+++ azure/terraform/main.tf
@@ -4,3 +4,12 @@ module "app" {
   environment = var.environment
   location = var.location
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "teststoragerg"
+    storage_account_name = "andreitest"
+    container_name       = "tfstate"
+    key                  = "azuretf.env.tfstate"
+  }
+}
