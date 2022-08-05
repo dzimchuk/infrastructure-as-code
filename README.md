473dad5030d7d6de5ca141fb5a435d5c8d99422f
Andrei Dzimchuk <3907708+dzimchuk@users.noreply.github.com>
2022-08-05 17:50:50 +0300

    use partial config for backend

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # infrastructure-as-code
 
 [Terraform variables](https://www.terraform.io/language/values/variables)
+[Partial backend configuration](https://www.terraform.io/language/settings/backends/configuration)
--- azure/terraform/main.tf
+++ azure/terraform/main.tf
@@ -4,12 +4,3 @@ module "app" {
   environment = var.environment
   location = var.location
 }
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = var.state_resource_group_name
-    storage_account_name = var.state_storage_account_name
-    container_name       = var.state_container_name
-    key                  = "azuretf.${var.environment}.tfstate"
-  }
-}
